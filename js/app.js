9dcfe04906ef4157d956b8b1438ae108cc47e0b6
it02inartec <it02@grupo-inartec.com.ve>
2014-07-08 16:52:07 -0430

    08/07/2014

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 //Definir un módulo angular de nuestra aplicación
-var app = angular.module('MyAppInartec', []);
+var app = angular.module('MyAppInartec', ['ngGrid']);
 
-app.controller('AreaControl', function($scope, $http) {
+app.controller('DashboardControl', function($scope, $http) {
 
   // Cargar todas las areas disponibles en BD
   getArea();
+
 
   function getArea(){
     $http.post("php/select.php").success(function(data){
       $scope.listareas = data;
     });
+    CallGrid();
   };
+
+  function CallGrid(){
+    debugger;
+    $scope.mySelections = [];
+    $scope.myData = [{Area: "MAIN LOBBY", Nombre: "Moroni", Apellido: "Perez", age: 50},
+                     {Area: "SALA DE ESPERA", Nombre: "Tiancum", Apellido: "Briceño", age: 43},
+                     {Area: "SALA DE ESPERA", Nombre: "Jacob", Apellido: "Alarcon", age: 27},
+                     {Area: "SALA DE ESPERA", Nombre: "Nephi", Apellido: "Beuses", age: 29},
+                     {Area: "RAXOS X", Nombre: "Enos", Apellido: "Barriga", age: 34}];
+    $scope.gridOptions = {
+      data: 'myData',
+      columnDefs: [{ field: "Area", displayName: 'Areas'},
+                    { field: "Nombre", displayName: 'Nombres'},
+                    { field: "Apellido", displayName: 'Apellidos'}],
+      selectedItems: $scope.mySelections,
+      multiSelect: false
+    };
+  }
 });
