5935897976b01beb0d8ea913b1294a436997dd9c
it02inartec <it02@grupo-inartec.com.ve>
2014-07-10 16:54:05 -0430

    10/07/2014

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Definir un módulo angular de nuestra aplicación
 var app = angular.module('MyAppInartec', ['ngGrid']);
-
+var efectoSelectedItems = true;
 app.controller('DashboardControl', function($scope, $http) {
 
   // Cargar todas las areas disponibles en BD
@@ -20,9 +20,22 @@ app.controller('DashboardControl', function($scope, $http) {
       { field: "nombre_area", displayName: 'Areas'},
       { field: "nombre", displayName: 'Nombres'},
       { field: "apellido", displayName: 'Apellidos'},
-      { field: "tiempo", displayName: 'Tiempo en Area'}
+      { field: "posicion", displayName: 'Posicion'}
     ],
+    // Para activar desplazamiento del div de los detalles del paciente y se vea con animacion.
+    afterSelectionChange: function() {
+      if(efectoSelectedItems == true){
+        $(".selectedItems").slideUp('slow').attr('ng-hide','true');
+        $(".selectedItems").slideDown('slow').attr('ng-hide','false');
+        efectoSelectedItems = false;
+      }
+      else{
+        efectoSelectedItems = true;
+      }
+    },
     selectedItems: $scope.mySelections,
+    showFilter: true,
+    filterOptions: {filterText:'', useExternalFilter: false},
     multiSelect: false
   }
 
@@ -33,10 +46,14 @@ app.controller('DashboardControl', function($scope, $http) {
   }
 
   $scope.cualArea = function (area) {
-    debugger;
-    /*$http.post("ajax/addTask.php?task="+task).success(function(data){
-      getTask();
-      $scope.taskInput = "";
-    });*/
+    $scope.gridOptions.filterOptions.filterText = area;
+  };
+
+  $scope.getDataOrder = function(){
+    //angular.forEach($scope.myData, function(data, index){   console.log(index,data); })
+    $.map($scope.gridOptions.ngGrid.filteredRows, function(o){
+      debugger;
+      console.log(o.entity);
+    });
   };
 });
