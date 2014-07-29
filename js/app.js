ed3e709152a75057bd973f99317300e1043c608c
it02inartec <it02@grupo-inartec.com.ve>
2014-07-29 16:54:08 -0430

    29/07/2014 (Mas pruebas con ui-sortable)

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
 //Definir un módulo angular de nuestra aplicación
-var app = angular.module('MyAppInartec', ['ngGrid', 'ui.sortable', 'ngRoute', 'angularFileUpload']);
+var app = angular.module('MyAppInartec', ['ngGrid', 'ui.sortable', 'ngRoute']);
 
 var efectoSelectedItems = true;
 var dataSelectedItem = [];
 var dataListArea = [];
 
-//var uploadUrl = 'http://angular-file-upload-cors-srv.appspot.com/upload';
-
-var uploadUrl = 'http://localhost/';
-
-window.uploadUrl = window.uploadUrl || 'localhost';
-
-app.controller('DashboardControl', function($scope, $http, $location, $timeout, $upload) {
+app.controller('DashboardControl', function($scope, $http, $location, $timeout) {
 
   // Cargar todas las areas disponibles en BD
   getArea();
 
   function getArea(){
     $http.post("php/areas.php").success(function(data){
-      $scope.listareas = data;
+      $scope.listareas = [data,data];
       LoadGrid();
-      dataListArea  = data;
     });
   };
 
@@ -58,7 +51,6 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout, 
     $http.post("php/consultas.php")
     .success(function(data){
       $scope.myData = data;
-      dataLis22  = data;
     });
   }
 
@@ -78,62 +70,39 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout, 
     $scope.mySelections = [];
   };
 
-  // Opciones/Eventos para el sortable..
+  // Inicio Opciones/Eventos para el sortable..
   $scope.sortingLog = [];
-  $scope.sortableOptions = {
-    activate: function() {
-        console.log("activate/activar");
-    },
-    beforeStop: function() {
-        console.log("beforeStop/antes de la parada");
-    },
-    change: function() {
-        console.log("change/cambio");
-    },
-    create: function() {
-        console.log("create/crear");
-    },
-    deactivate: function() {
-        console.log("deactivate/desactivar");
-    },
-    out: function() {
-        console.log("out/fuera");
-    },
-    over: function() {
-        console.log("over/encima");
-    },
-    receive: function() {
-        console.log("receive/recibir");
-    },
-    remove: function() {
-        console.log("remove/eliminar");
-    },
-    sort: function() {
-        console.log("sort/ordenar");
-    },
-    start: function() {
-        console.log("start/comienzo");
-    },
-    update: function(e, ui) {
-      console.log("--> Levantaste a '"+ui.item[0].innerText+"'<--");
-      console.log("update/actualización");
 
-      var logEntry = dataListArea.map(function(i){
-        return i.nombre_area;
-      }).join(', ');
-      $scope.sortingLog.push('* Update= ' + logEntry);
-    },
-    stop: function(e, ui) {
-      console.log("stop/parada");
+  function createOptions (listName) {
+    var _listName = listName;
+    var options = {
+      placeholder: "Jen2",
+      connectWith: ".Jen",
+      stop: function() {
+          console.log("lista " + _listName + ": parada");
+      },
+      update: function() {
+          console.log("lista " + _listName + ": actualiza");
+          $scope.logModels();
+      }
+    };
+    return options;
+  }
+  $scope.sortableOptions = [createOptions('A'), createOptions('B')];
 
-      // Esta devolucion tiene el modelo cambiado
-      var logEntry = dataListArea.map(function(i){
-        return i.nombre_area;
+  $scope.logModels = function () {
+    $scope.sortingLog = [];
+    for (var i = 0; i < $scope.listareas.length; i++) {
+      var logEntry = $scope.listareas[i].map(function (x) {
+        return x.nombre_area;
       }).join(', ');
-      $scope.sortingLog.push('* Stop= ' + logEntry);
+      logEntry = 'Contenedor ' + (i+1) + ': ' + logEntry;
+      $scope.sortingLog.push(logEntry);
     }
   };
+  // Fin Opciones/Eventos para el sortable..
 
+  // Data grid del cuadro detalles del paciente
   $scope.myData2 = [
     {sintoma: "1 Moroni", procedimiento:'test 1', estatus:0},
     {sintoma: "2 Moroni", procedimiento:'test 2', estatus:0},
@@ -142,6 +111,7 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout, 
     {sintoma: "5 Moroni", procedimiento:'test 5', estatus:0}
   ];
 
+  // Opciones del grid del cuadro detalles del paciente
   $scope.mySelections2 = [];
   $scope.gridOptions2 = {
     showSelectionCheckbox: true,
@@ -158,11 +128,12 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout, 
     multiSelect: true
   }
 
+  // recorro evento del grid del cuadro detalles del paciente
   $scope.$on('ngGridEventData', function(){
     // Recorro los datos de "myData2".
     angular.forEach($scope.myData2, function(data, index){
         // si estatus es igual a uno marco la fila.
-        if(data.estatus == 1){
+        if(data.estatus === 1){
           // index = posicion de la fila que sera marcada.
           $scope.gridOptions2.selectItem(index, true);
         }
@@ -178,119 +149,9 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout, 
     });
   };
 
+  // Para cambiar de pagina html
   $scope.go = function (path){
     $location.path(path);
-  };
-
-  /* Inicio cargar archivos */
-  $scope.usingFlash = FileAPI && FileAPI.upload != null;
-  $scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
-  $scope.uploadRightAway = true;
-  $scope.changeAngularVersion = function() {
-    window.location.hash = $scope.angularVersion;
-    window.location.reload(true);
-  };
-  $scope.hasUploader = function(index) {
-    return $scope.upload[index] != null;
-  };
-  $scope.abort = function(index) {
-    $scope.upload[index].abort();
-    $scope.upload[index] = null;
-  };
-  $scope.angularVersion = window.location.hash.length > 1 ? (window.location.hash.indexOf('/') === 1 ? window.location.hash.substring(2): window.location.hash.substring(1)) : '1.2.20';
-  $scope.onFileSelect = function($files) {
-    $scope.selectedFiles = [];
-    $scope.progress = [];
-    if ($scope.upload && $scope.upload.length > 0) {
-      for (var i = 0; i < $scope.upload.length; i++) {
-        if ($scope.upload[i] != null) {
-          $scope.upload[i].abort();
-        }
-      }
-    }
-    $scope.upload = [];
-    $scope.uploadResult = [];
-    $scope.selectedFiles = $files;
-    $scope.dataUrls = [];
-    for ( var i = 0; i < $files.length; i++) {
-      var $file = $files[i];
-      if ($scope.fileReaderSupported && $file.type.indexOf('image') > -1) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL($files[i]);
-        var loadFile = function(fileReader, index) {
-          fileReader.onload = function(e) {
-            $timeout(function() {
-              $scope.dataUrls[index] = e.target.result;
-            });
-          }
-        }(fileReader, i);
-      }
-      $scope.progress[i] = -1;
-      if ($scope.uploadRightAway) {
-        $scope.start(i);
-      }
-    }
-  };
-  $scope.start = function(index) {
-    $scope.progress[index] = 0;
-    $scope.errorMsg = null;
-    if ($scope.howToSend == 1) {
-      $scope.upload[index] = $upload.upload({
-        url: uploadUrl,
-        method: $scope.httpMethod,
-        headers: {'my-header': 'my-header-value'},
-        data : {
-          myModel : $scope.myModel
-        },
-        file: $scope.selectedFiles[index],
-        fileFormDataName: 'myFile'
-      });
-      $scope.upload[index].then(function(response) {
-        $timeout(function() {
-          $scope.uploadResult.push(response.data);
-        });
-      }, function(response) {
-        if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-      }, function(evt) {
-        // Math.min is to fix IE which reports 200% sometimes
-        $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-      });
-      $scope.upload[index].xhr(function(xhr){
-        // xhr.upload.addEventListener('abort', function() {console.log('abort complete')}, false);
-      });
-    } else {
-      var fileReader = new FileReader();
-            fileReader.onload = function(e) {
-            $scope.upload[index] = $upload.http({
-              url: uploadUrl,
-          headers: {'Content-Type': $scope.selectedFiles[index].type},
-          data: e.target.result
-            }).then(function(response) {
-          $scope.uploadResult.push(response.data);
-        }, function(response) {
-          if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-        }, function(evt) {
-          // Math.min is to fix IE which reports 200% sometimes
-          $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-        });
-            }
-          fileReader.readAsArrayBuffer($scope.selectedFiles[index]);
-    }
-  };
-  $scope.dragOverClass = function($event) {
-    var items = $event.dataTransfer.items;
-    var hasFile = false;
-    if (items != null) {
-      for (var i = 0 ; i < items.length; i++) {
-        if (items[i].kind == 'file') {
-          hasFile = true;
-          break;
-        }
-      }
-    } else {
-      hasFile = true;
-    }
-    return hasFile ? "dragover" : "dragover-err";
   };
 });
 
@@ -357,15 +218,3 @@ function Create_StadistDay(Day,PerDay,DataGrap,container){
     }
   });
 }
-
-FileAPI = {
-  debug: true,
-  //forceLoad: true, html5: false //to debug flash in HTML5 browsers
-  //only one of jsPath or jsUrl.
-    //jsPath: '/js/FileAPI.min.js/folder/',
-    //jsUrl: 'yourcdn.com/js/FileAPI.min.js',
-
-    //only one of staticPath or flashUrl.
-    //staticPath: '/flash/FileAPI.flash.swf/folder/'
-    //flashUrl: 'yourcdn.com/js/FileAPI.flash.swf'
-};
