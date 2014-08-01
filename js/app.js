2a16cb8b067d019addc5efe743f0883e9d64a389
it02inartec <it02@grupo-inartec.com.ve>
2014-08-01 15:44:02 -0430

    01/08/2014

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Definir un módulo angular de nuestra aplicación
+//Definir un módulo angular de nuestra aplicación.
 var app = angular.module('MyAppInartec', ['ngGrid', 'ui.sortable', 'ngRoute']);
 
 var efectoSelectedItems = true;
@@ -7,32 +7,57 @@ var dataListArea = [];
 
 app.controller('DashboardControl', function($scope, $http, $location, $timeout) {
 
-  // Cargar todas las areas disponibles en BD
+  // Cargar todas las areas disponibles en BD.
   getArea();
 
   function getArea(){
     $http.post("php/areas.php").success(function(data){
       $scope.listareas = data;
+      // Creamos datos para prueba de sortables.
       esto = [[{ id: '1', nombre_area: 'Jose' },
                { id: '2', nombre_area: 'Laura' },
                { id: '3', nombre_area: 'Maria' },
-               { id: '4', nombre_area: 'Miguel' }],
-            [{ id: '5', nombre_area: 'Raul' },
-               { id: '6', nombre_area: 'Brenda' },
-               { id: '7', nombre_area: 'Palmariz' },
-               { id: '8', nombre_area: 'Johanna' }],
-            [{ id: '9', nombre_area: 'Angeline' },
+               { id: '4', nombre_area: 'Miguel' },
+               { id: '5', nombre_area: 'Palmariz' },
+               { id: '6', nombre_area: 'Johanna' }],
+            [/*{ id: '1', nombre_area: 'Jose' }*/],
+            [{ id: '7', nombre_area: 'Angeline' },
+               { id: '8', nombre_area: 'Eleazar' }],
+            [{ id: '9', nombre_area: 'Raul' },
                { id: '10', nombre_area: 'Jackeline' },
-               { id: '11', nombre_area: 'Eleazar' },
+               { id: '11', nombre_area: 'Brenda' },
                { id: '12', nombre_area: 'Graciela' }]];
       $scope.listareasS = esto;
+      // Llamamos metodo para cargar el grid de "Cuadro de Pacientes".
       LoadGrid();
     });
   };
 
   // Valores para la paginacion
   $scope.totalServerItems = 0;
-  $scope.pagingOptions = { pageSizes: [2, 5, 10], pageSize: 2, currentPage: 1 };
+  $scope.pagingOptions = {
+    pageSizes: [3, 10, 15], // Tamaños de paginas.
+    pageSize: 3, // Tamaño de pagina inicial.
+    currentPage: 1 // Pagina actual.
+  };
+  // Funcion que hace el calculo para la paginacion al darle next o preview.
+  $scope.setPagingData = function(data, page, pageSize){
+    var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+    $scope.myData = pagedData;
+    $scope.totalServerItems = data.length;
+    if (!$scope.$$phase) {
+        $scope.$apply();
+    }
+  };
+  // Sirve para capturar valores luego del evento en el boton next o preview.
+  $scope.$watch('pagingOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+          $scope.setPagingData($scope.myDataTotal,$scope.pagingOptions.currentPage,$scope.pagingOptions.pageSize);
+        }
+        if (newVal !== oldVal && newVal.pageSize !== oldVal.pageSize) {
+          $scope.setPagingData($scope.myDataTotal,$scope.pagingOptions.currentPage,$scope.pagingOptions.pageSize);
+        }
+    }, true);
 
   // Primero hay que definir las opciones el grid y sus columnas.
   $scope.mySelections = [];
@@ -47,7 +72,7 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout) 
     // Para activar desplazamiento del div de los detalles del paciente y se vea con animacion.
     afterSelectionChange: function() {
       if(efectoSelectedItems == true){
-        $(".selectedItems").height('400px');
+        $(".selectedItems").height('380px');
         $scope.mySelections = dataSelectedItem;
         $(".selectedItems").slideUp();
         $(".selectedItems").slideDown(1000,'linear');
@@ -61,30 +86,32 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout) 
     showFilter: true,
     filterOptions: {filterText:'', useExternalFilter: false},
     multiSelect: false,
+    // Para paginacion -->
     enablePaging: true,
     showFooter: true,
+    footerRowHeight: 40,
     totalServerItems: 'totalServerItems', // Para validar cuantos mas "next" se pueden dar.
     pagingOptions: $scope.pagingOptions
+    // <--
   }
 
-  // Luego podemos cargar el grid
+  // Luego podemos cargar el grid.
   function LoadGrid(){
     $http.post("php/consultas.php")
     .success(function(data){
-      $scope.myData = data;
-      debugger;
+      $scope.myDataTotal = data;
       var pagedData = data.slice(($scope.pagingOptions.currentPage - 1) * $scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage * $scope.pagingOptions.pageSize);
       $scope.myData = pagedData;
       $scope.totalServerItems = data.length;
     });
   }
 
-  // Para filtrar pacientes por area (MobileList)
+  // Para filtrar pacientes por area (MobileList).
   $scope.cualArea = function (area) {
     $scope.gridOptions.filterOptions.filterText = area;
   };
 
-  // Para carturar array del grid filtrado
+  // Para carturar array del grid filtrado.
   $scope.getDataOrder = function(){
     $.map($scope.gridOptions.ngGrid.filteredRows, function(o){
       console.log(o.entity);
@@ -94,6 +121,58 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout) 
     dataSelectedItem = $scope.gridOptions.selectedItems;
     $scope.mySelections = [];
   };
+
+  // ---------------- Cuadro detalles del paciente ---------------- //
+
+  // Data grid del cuadro detalles del paciente.
+  $scope.myData2 = [
+    {sintoma: "1 Moroni", procedimiento:'test 1', estatus:0},
+    {sintoma: "2 Moroni", procedimiento:'test 2', estatus:0},
+    {sintoma: "3 Moroni", procedimiento:'test 3', estatus:0},
+    {sintoma: "4 Moroni", procedimiento:'test 4', estatus:0},
+    {sintoma: "5 Moroni", procedimiento:'test 5', estatus:0}
+  ];
+
+  // Opciones del grid del cuadro detalles del paciente.
+  $scope.mySelections2 = [];
+  $scope.gridOptions2 = {
+    showSelectionCheckbox: true,
+    data: 'myData2',
+    columnDefs: [
+      { field: "sintoma", displayName: 'Sintoma'},
+      { field: "procedimiento", displayName: 'Procediemiento'},
+      { field: "estatus", displayName: 'Estatus'}
+    ],
+    selectedItems: $scope.mySelections2,
+    showFilter: true,
+    filterOptions: {filterText:'', useExternalFilter: false},
+    sortInfo:  { fields: ['estatus'], directions: ['asc'] },
+    multiSelect: true
+  }
+
+  // recorro evento del grid del cuadro detalles del paciente.
+  $scope.$on('ngGridEventData', function(){
+    // Recorro los datos de "myData2".
+    angular.forEach($scope.myData2, function(data, index){
+        // si estatus es igual a uno marco la fila.
+        if(data.estatus === 1){
+          // index = posicion de la fila que sera marcada.
+          $scope.gridOptions2.selectItem(index, true);
+        }
+    });
+  });
+
+  $scope.Arriba = function(){
+    angular.forEach($scope.myData2, function(data, index){
+      debugger;
+        if($scope.mySelections2.length !==0 && data.procedimiento === $scope.mySelections2[0].procedimiento){
+          // Cambio el valor de una fila por "1".
+          $scope.myData2[index].estatus = 1;
+        }
+    });
+  };
+
+  // ------------------- Drag&Drop (pagina sortable) --------------------- //
 
   // Inicio Opciones/Eventos para el sortable..
   /*$scope.sortingLog = [];
@@ -125,12 +204,13 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout) 
   };*/
 
   $scope.sortingLog = [];
-  // Configuramos que listas estaran conectadas
+  // Configuramos que listas estaran conectadas.
   $scope.sortableOptions = {
     placeholder: "Jen2", // Llamamos la clase del ul que queremos conectar.
     connectWith: ".Jen", // Llamamos la clase del li que queremos conectar.
     update: function(event, ui) {
-      if (event.target.id !== 'sort-1' && ui.item.sortable.droptarget.attr('id') === 'sort-1' && $scope.listareasS[1].length >= 5) {
+      debugger;
+      if (event.target.id !== 'sort-2' && ui.item.sortable.droptarget.attr('id') === 'sort-2' && $scope.listareasS[2].length >= 2) {
         ui.item.sortable.cancel();
         alert('Capacidad Maxima Alcanzada');
       }
@@ -149,60 +229,28 @@ app.controller('DashboardControl', function($scope, $http, $location, $timeout) 
   };
   // Fin Opciones/Eventos para el sortable..
 
-  // Data grid del cuadro detalles del paciente
-  $scope.myData2 = [
-    {sintoma: "1 Moroni", procedimiento:'test 1', estatus:0},
-    {sintoma: "2 Moroni", procedimiento:'test 2', estatus:0},
-    {sintoma: "3 Moroni", procedimiento:'test 3', estatus:0},
-    {sintoma: "4 Moroni", procedimiento:'test 4', estatus:0},
-    {sintoma: "5 Moroni", procedimiento:'test 5', estatus:0}
-  ];
+  // ------------------- Drag&Drop (pagina cuadros) --------------------- //
 
-  // Opciones del grid del cuadro detalles del paciente
-  $scope.mySelections2 = [];
-  $scope.gridOptions2 = {
-    showSelectionCheckbox: true,
-    data: 'myData2',
-    columnDefs: [
-      { field: "sintoma", displayName: 'Sintoma'},
-      { field: "procedimiento", displayName: 'Procediemiento'},
-      { field: "estatus", displayName: 'Estatus'}
-    ],
-    selectedItems: $scope.mySelections2,
-    showFilter: true,
-    filterOptions: {filterText:'', useExternalFilter: false},
-    sortInfo:  { fields: ['estatus'], directions: ['asc'] },
-    multiSelect: true
-  }
-
-  // recorro evento del grid del cuadro detalles del paciente
-  $scope.$on('ngGridEventData', function(){
-    // Recorro los datos de "myData2".
-    angular.forEach($scope.myData2, function(data, index){
-        // si estatus es igual a uno marco la fila.
-        if(data.estatus === 1){
-          // index = posicion de la fila que sera marcada.
-          $scope.gridOptions2.selectItem(index, true);
-        }
-    });
-  });
-
-  $scope.Arriba = function(){
-    angular.forEach($scope.myData2, function(data, index){
-        if(data.procedimiento === $scope.mySelections2[0].procedimiento){
-          // Cambio el valor de una fila por "1".
-          $scope.myData2[index].estatus = 1;
-        }
-    });
+  //
+  $scope.sortingLog2 = [];
+  // Configuramos que listas estaran conectadas.
+  $scope.sortableOptions2 = {
+    placeholder: "Jen3", // Llamamos la clase del ul que queremos conectar.
+    connectWith: ".contenedor", // Llamamos la clase del li que queremos conectar.
+    update: function(event, ui) {
+      debugger;
+    }
   };
 
-  // Para cambiar de pagina html
+  // ------------- Metodo para los botones de navegacion -------------- //
+
+  // Para cambiar de pagina html.
   $scope.go = function (path){
     $location.path(path);
   };
 });
 
-// Configuración de las rutas
+// Configuración de las rutas.
 app.config(function($routeProvider){
     $routeProvider
         .when('/', {
@@ -211,6 +259,10 @@ app.config(function($routeProvider){
         })
         .when('/sortable', {
             templateUrl : 'pages/sortable.html',
+            controller  : 'DashboardControl'
+        })
+        .when('/cuadros', {
+            templateUrl : 'pages/cuadros.html',
             controller  : 'DashboardControl'
         })
         .otherwise({
